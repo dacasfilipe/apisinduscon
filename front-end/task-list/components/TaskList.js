b82f905f0edf365b74d4915c07096c5dde9277ef
dacasfilipe <filiperdacas@gmail.com>
2023-06-07 23:26:51 -0300

    atualizado componente tasklist

--- front-end/task-list/components/TaskList.js
+++ front-end/task-list/components/TaskList.js
@@ -12,7 +12,7 @@ const TaskList = () => {
     const [modalIsOpen, setModalIsOpen] = useState(false);
     const [tasks, setTasks] = useState([]);
     const [page, setPage] = useState(1);
-    const [currentTask, setCurrentTask] = useState(null);
+    const [currentTaskId, setCurrentTaskId] = useState(null);
     const [loading, setLoading] = useState(false);
 
     //hook que serve para controlar efeitos colaterais de comportamento do componente
@@ -71,9 +71,52 @@ const TaskList = () => {
     //O retorno é inserido em JSX que utiliza HTML para ser renderizado dentro do javascript
     //o return de um componente react deve sempre retornar um único elemento, seja ele uma section, uma div ou um elemento vazio
     return (
-        <>
-        
-        </>
+        <div className="main">
+            <h1>Lista de Tarefas</h1>
+            <div className="button-new-task-container">
+                <button className="sucess" onClick={() => handleNew()}>Nova Tarefa</button>
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Descrição</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tasks.map((task) => (
+                        <tr key={task.id}>
+                            <td>{task.id}</td>
+                            <td>{task.description}</td>
+                            <td>
+                                <button onClick={() => handleEdit(task.id)}>Editar</button>
+                                <button onClick={() => handleDelete(task.id)}>Excluir</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div className="load-button-container">
+                {loading ?(
+                    <p>Carregando...</p>
+                ):(
+                    <button onClick={loadMore}>Carregar mais</button>
+                )}
+            </div>
+
+            <Modal 
+                className="modal"
+                ariaHideApp={false}
+                isOpen={modalIsOpen}
+                onRequestClose={handleCloseModal}
+            >
+                <h2>{currentTaskId ? "Editar Tarefa" : "Nova Tarefa"}</h2>
+                <TaskForm id={currentTaskId} onSave={handleSave}/>
+            </Modal>
+            <ToastContainer />
+        </div>
     )
 
     
