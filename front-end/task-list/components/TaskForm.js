912ce60880b16f91949ed0aafb1523849948dce4
dacasfilipe <filiperdacas@gmail.com>
2023-06-09 12:57:32 -0300

    atualizado componente taskform e suas funções juntamento com formulário de retorno

--- front-end/task-list/components/TaskForm.js
+++ front-end/task-list/components/TaskForm.js
@@ -12,5 +12,45 @@ const TaskForm = (props) => {
             setTask(task);
         };
         load();
-    })
-}
+    },[id]);
+
+    const handleChange = (e) => {
+        setTask({
+            ...task,
+            description:e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(props.id){
+            TaskService.updateTask(props.id, task)
+            .then(()=>{
+                props.onSave();
+            })
+            .catch((error)=>{
+                console.log(error);
+            });
+        }else{
+            TaskService.createTask(task)
+            .then(()=>{
+                props.onSave();
+            })
+            .catch((error)=>{
+                console.log(error);
+            })
+        }
+    };
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" 
+                name="description"
+                placeholder="Task description"
+                onChange={handleChange}
+                value={task.description ? task.description:""} />
+            <button type="submit">Save</button>
+        </form>
+    );
+};
+
+export default TaskForm;
